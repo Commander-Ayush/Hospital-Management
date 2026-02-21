d2285842e875ed8b0f9eac6d95404c7a7b235341
Commander-Ayush <ayushkrjha221004@gmail.com>
2026-02-21 15:32:19 +0530

    corrected the application.properties according to supabase

--- src/main/java/com/hospital/app/hospitalmanagement/entity/Patient.java
+++ src/main/java/com/hospital/app/hospitalmanagement/entity/Patient.java
@@ -1,6 +1,8 @@
 package com.hospital.app.hospitalmanagement.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 
@@ -8,6 +10,11 @@ import lombok.Data;
 @Data
 public class Patient {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long PatientId;
+    private String name;
+    private int age;
+    private String gender;
+
 
 }
